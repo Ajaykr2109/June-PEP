af6b4aa0deac41757cb9195ef0f482e2eed81c39
Ajay Kumar Chaturvedi <chaturvedikraj2109@gmail.com>
2022-10-11 10:36:15 +0530

    up

---
+++ atm.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// int main()
+// {
+//     string name;
+//     int balance;
+//     int withdraw;
+
+//     cout << "Enter the Name and Balance of the Account Holder: ";
+//     cin >> name >> balance;
+//     cout << "Enter the amount to be withdrawn:";
+//     cin >> withdraw;
+
+//     if (balance - withdraw >= 1000)
+//     {
+//         cout << "Withdrawal Successful" << endl;
+//         cout << "Balance: " << balance - withdraw;
+//     }
+//     else
+//     {
+//         cout << "Insufficient Balance";
+//     }
+
+//     return 0;
+// }
+
+// class goldenPG
+// {
+
+// public:
+//     string name;
+//     int roomno;
+//     int rent = 7500;
+
+//     void getDetails()
+//     {
+//         cout << "Enter the Name of the Holder: ";
+//         cin >> name;
+//         cout << "Enter the Room:";
+//         cin >> roomno;
+//     }
+// };
+
+// int main()
+// {
+//     int num;
+//     cin >> num;
+//     goldenPG g[num];
+
+//     for (int i = 0; i < num; i++)
+//     {
+//         g[i].getDetails();
+//     }
+
+//     for (int i = 0; i < num; i++)
+//     {
+//         cout << g[i].name << " " << g[i].roomno << " " << g[i].rent << endl;
+//     }
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int arr[10];
+
+//     for (int i = 0; i < 10; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     for (int i = 0; i < 10; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+// }
